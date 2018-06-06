17a41ec6b58593b05621e97cf4f2309cec5e59bb
Peter Zhang <pzhang87@gmail.com>
2018-06-05 21:02:46 -0400

    add seeds

---
+++ seeders/20180606004317-banners.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('Person', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Banners',
+      [
+        {
+          name: 'TL Soku Base',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+        {
+          name: 'DKPL Special',
+          createdAt: new Date(),
+          updatedAt: new Date()
+        }
+      ],
+    {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('Person', null, {});
+    */
+
+    return queryInterface.bulkDelete('Banner', null, {});
+  }
+};
