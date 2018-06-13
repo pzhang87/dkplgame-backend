3b15186f2e13ca37f3cb12f9d8423e62f503bd32
Peter Zhang <pzhang87@gmail.com>
2018-06-13 18:58:55 -0400

    remember to delete comments

--- seeders/20180606004317-banners.js
+++ seeders/20180606004317-banners.js
@@ -2,16 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('Person', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
     return queryInterface.bulkInsert('Banners',
       [
         {
@@ -29,14 +19,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('Person', null, {});
-    */
-
     return queryInterface.bulkDelete('Banner', null, {});
   }
 };
