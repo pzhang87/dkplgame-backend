7380a885f627e32295759c4c98002552deb9842c
Peter Zhang <pzhang87@gmail.com>
2018-06-19 20:48:34 -0400

    roll logic

--- models/user.js
+++ models/user.js
@@ -1,4 +1,7 @@
 'use strict';
+
+const BANNERS = require('../config/20180614.json').banners;
+
 module.exports = function(orm, { INTEGER, STRING, BOOLEAN, ...Sequelize}){
   const User = orm.define('user', {
     discordID: {
@@ -32,7 +35,7 @@ module.exports = function(orm, { INTEGER, STRING, BOOLEAN, ...Sequelize}){
 
   User.prototype.roll = function(args){
     // wrap all of this shit in try/catch block at the end pls
-    let { banner, rolls } = args
+    let { bannerId, rolls } = args
     if (this.rocks >= rolls){
       // generate an array of rolls
       // ES6 implementation. isn't this cool?
@@ -41,6 +44,17 @@ module.exports = function(orm, { INTEGER, STRING, BOOLEAN, ...Sequelize}){
       // have something parse this array into odds: for each element in array, decide
       // on whether or not it is N/R/SR/SSR based on the bannerOdds
       // this will necessitate a banner(global)Odds table
+
+      let odds = {
+        "N": [0, BANNERS[bannerId]["odds"]["N"] - 1],
+        "R": [BANNERS[bannerId]["odds"]["N"], 99 - BANNERS[bannerId]["odds"]["R"]],
+        "SR": [100 - BANNERS[bannerId]["odds"]["SR"], 99 - BANNERS[bannerId]["odds"]["SSR"]],
+        "SSR": [100 - BANNERS[bannerId]["odds"]["SSR"], 99]
+      }
+
+      pulls.map((pull) => {
+
+      })
 
       // then, determine the identity of each card, via each individual card's odds
       // as determined on bannerCardsOdds
